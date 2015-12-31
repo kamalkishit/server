58a70fcee15aba13ca03e58d81321ccc8b7ad8fc
kamal <pandey.kishore@gmail.com>
2015-12-31 18:44:14 +0530

    more changes

--- src/main/java/com/humanize/server/authentication/service/EmailServiceImpl.java
+++ src/main/java/com/humanize/server/authentication/service/EmailServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.mail.MessagingException;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.apache.velocity.Template;
@@ -38,34 +40,43 @@ public class EmailServiceImpl implements EmailService{
 	public boolean sendEmail(Message message) throws SendEmailException {
 		mimeMessage = javaMailSender.createMimeMessage();
 		try {
+			
+			//send(message.getTo());
 			mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
 			mimeMessageHelper.setTo(message.getTo());
 			mimeMessageHelper.setSubject(message.getSubject());
-			//mimeMessageHelper.setText(message.getBody());
-			
-			/*VelocityEngine ve = new VelocityEngine();
-	        ve.init();
-	        Template t = ve.getTemplate("invitecode.vm");
-	        VelocityContext context = new VelocityContext();
-	        context.put("name", "World");
-	        StringWriter writer = new StringWriter();
-	        t.merge( context, writer );
-	        
-	        mimeMessageHelper.setText(writer.toString());*/
-	        
-	        Map<String, Object> model = new HashMap<String, Object>();
-			String str = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine,
-					"invitecode.vm", model);
-			
-			mimeMessageHelper.setText(str, true);
+			mimeMessageHelper.setText(message.getBody());
 			
 			javaMailSender.send(mimeMessage);
 			
 		} catch(MessagingException exception) {
 			logger.error("", exception);
 			throw new SendEmailException(0, null);
+		} catch (Exception exception) {
+			throw new SendEmailException(0, null);
 		}
 		
 		return true;
+	}
+	
+	private void send(String emailId) throws Exception {
+		mimeMessage = javaMailSender.createMimeMessage();
+	  Template template = velocityEngine.getTemplate("./invitecode.vm");
+	
+	  VelocityContext velocityContext = new VelocityContext();
+	   
+	  StringWriter stringWriter = new StringWriter();
+	  
+	  template.merge(velocityContext, stringWriter);
+	  
+	  mimeMessage.setFrom(new InternetAddress("kamal@humannize.com"));
+	  mimeMessage.setRecipients(javax.mail.Message.RecipientType.TO, emailId);
+	  mimeMessage.setSubject("Welcome to Triplived, a platform to share and relive memorable travel experiences");
+	  
+	  mimeMessage.setText(stringWriter.toString());
+	  mimeMessage.setContent(stringWriter.toString(), "text/html");
+	  
+	  //mailSender.send(message);
+	  javaMailSender.send(mimeMessage);
 	}
 }
